05d9ca3b53a89c099c781635103045f42f04b171
Proladon <Proladon@gmail.com>
2023-02-20 22:34:36 +0800

    TeamMembers / TeamMemberCard

--- src/views/Home/components/TeamIntro/components/TeamMemberCard.vue
+++ src/views/Home/components/TeamIntro/components/TeamMemberCard.vue
@@ -1,20 +1,39 @@
 <template>
-  <SZHighLightContainer class="team-member-card">
-    <img :src="data.avatar" alt="" />
-    <div class="info">
-      <p class="team-member-name">{{ data.name }}</p>
-      <div class="team-member-tags">
-        <NTag
-          :bordered="false"
-          v-for="tag in data.tags"
-          :key="tag"
-          :color="tagTypes[tag]?.color"
-        >
-          {{ tag }}
-          <template #icon>
-            <NIcon :size="14" :component="tagTypes[tag]?.icon" />
-          </template>
-        </NTag>
+  <SZHighLightContainer
+    class="team-member-card"
+    :color="data?.color"
+    glow="4.5"
+    lightWidth="7"
+  >
+    <div class="content-wrapper">
+      <img class="team-member-avatar" :src="data?.avatar" alt="" />
+      <div class="info">
+        <p class="team-member-name">{{ data?.name }}</p>
+        <div class="team-member-links">
+          <a
+            v-for="link in data?.links || []"
+            :key="link.name"
+            :href="link.link"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="link.name"
+          >
+            <NIcon :size="20" :component="link.icon" />
+          </a>
+        </div>
+        <div class="team-member-tags">
+          <NTag
+            :bordered="false"
+            v-for="tag in data?.tags || []"
+            :key="tag"
+            :type="tagTypes[tag]?.type"
+          >
+            {{ tag }}
+            <template #icon>
+              <NIcon :size="14" :component="tagTypes[tag]?.icon" />
+            </template>
+          </NTag>
+        </div>
       </div>
     </div>
   </SZHighLightContainer>
@@ -27,8 +46,6 @@ import {
   FlashFilled,
   StarFilled,
   IbmCloudPakWatsonAiops,
-  Integration,
-  Layers,
   Chemistry,
 } from '@vicons/carbon'
 
@@ -39,29 +56,43 @@ defineProps({
 const tagTypes = {
   fullStack: {
     icon: StarFilled,
-    color: { color: 'rgba(95, 246, 180, 0.3)' },
+    color: { color: 'rgba(99, 226, 183, 0.16)' },
+    type: 'info',
   },
   frontend: {
     icon: FlashFilled,
     color: { color: 'rgba(95, 246, 180, 0.3)' },
+    type: 'warning',
   },
   backend: {
     icon: Chemistry,
     color: { color: 'rgba(95, 246, 180, 0.3)' },
+    type: 'success',
   },
   ops: {
     icon: IbmCloudPakWatsonAiops,
     color: { color: 'rgba(95, 246, 180, 0.3)' },
+    type: 'error',
   },
 }
 </script>
 
 <style scoped lang="postcss">
-.team-member-card {
+.content-wrapper {
+  @apply flex gap-[20px];
+}
+
+.team-member-avatar {
+  @apply max-w-[90px] h-full rounded-md;
 }
 
 .team-member-name {
   @apply font-medium;
+}
+
+.team-member-links {
+  @apply flex gap-[10px];
+  @apply mb-[5px];
 }
 
 .team-member-tags {
--- src/views/Home/components/TeamIntro/components/TeamMembers.vue
+++ src/views/Home/components/TeamIntro/components/TeamMembers.vue
@@ -10,11 +10,66 @@
 
 <script setup lang="ts">
 import TeamMemberCard from './TeamMemberCard.vue'
+import { LogoGithub, LogoYoutube } from '@vicons/carbon'
 
 const members = [
-  { name: 'Proladon#7525', tags: ['fullStack'] },
-  { name: '星曌#4316', tags: ['backend'] },
-  { name: 'Irvine#8850', tags: ['frontend', 'ops'] },
+  {
+    name: 'Proladon#7525',
+    tags: ['fullStack'],
+    color: '#55B0F2',
+    links: [
+      { name: 'github', icon: LogoGithub, link: 'https://github.com/Proladon' },
+      {
+        name: 'youtube',
+        icon: LogoYoutube,
+        link: 'https://www.youtube.com/@Proladon',
+      },
+    ],
+    avatar:
+      'https://cdn.discordapp.com/avatars/149772971555160064/7ab2f70347204922ec90612abc432b0a.png?size=1024',
+  },
+  {
+    name: '星曌#4316',
+    tags: ['backend'],
+    color: '#63E2B7',
+    links: [
+      {
+        name: 'github',
+        icon: LogoGithub,
+        link: 'https://github.com/Xzihnago',
+      },
+    ],
+    avatar:
+      'https://cdn.discordapp.com/avatars/470034188326797320/65a8f2c7d87c5ae5560f3f58babfdcbb.png?size=1024',
+  },
+  {
+    name: 'Irvine#8850',
+    tags: ['frontend', 'ops'],
+    color: '#F2E355',
+    links: [
+      {
+        name: 'github',
+        icon: LogoGithub,
+        link: 'https://github.com/irvine1231',
+      },
+    ],
+    avatar:
+      'https://cdn.discordapp.com/avatars/175464140825952256/a9cfcb8608c084ebeed2ccae18d284dd.png?size=1024',
+  },
+  {
+    name: 'Ariz#0288',
+    tags: ['frontend', 'ops'],
+    color: '#F2E355',
+    links: [
+      {
+        name: 'github',
+        icon: LogoGithub,
+        link: 'https://github.com/Lunari8546',
+      },
+    ],
+    avatar:
+      'https://cdn.discordapp.com/avatars/443715689291841536/767defce25d4cd57734f623108519308.png?size=1024',
+  },
 ]
 </script>
 
