9e1aab487b23d821a6f84c9642849c31c8a1f384
Proladon <Proladon@gmail.com>
2023-05-03 18:24:06 +0800

    [wip] ResourceCard

---
+++ src/views/Ecosystem/Ecosystem.vue
@@ -0,0 +1,55 @@
+<template>
+  <main class="applications">
+    <div class="container">
+      <ResourceCard v-for="item in applications" :key="item.name" :data="item">
+        <h3>{{ item.name }}</h3>
+      </ResourceCard>
+    </div>
+    <div class="container">
+      <ResourceCard v-for="item in docs" :key="item.name" :data="item">
+        <h3>{{ item.name }}</h3>
+      </ResourceCard>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import ResourceCard from './components/ResourceCard.vue'
+const applications = [
+  {
+    name: 'SZ-Form',
+    description: '',
+    link: 'https://shelter-zone.github.io/sz-form-frontend/',
+  },
+  {
+    name: 'SZ Question Template Generator',
+    description: '',
+    link: 'https://shelter-zone.github.io/Question-Template/',
+  },
+]
+
+const docs = [
+  {
+    name: 'Technological Exchange Guide',
+    description: '',
+    link: 'https://shelter-zone.github.io/Technological-Exchange-Guide/',
+  },
+  {
+    name: 'Discord Bot FAQ',
+    description: '',
+    link: 'https://shelter-zone.github.io/Discord_Bot_FAQ/',
+  },
+]
+</script>
+
+<style scoped lang="postcss">
+.applications {
+  @apply pt-[100px];
+  @apply flex flex-col items-center gap-[60px];
+}
+
+.container {
+  @apply w-full;
+  @apply flex gap-[30px];
+}
+</style>
---
+++ src/views/Ecosystem/components/ResourceCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <a
+    class="resource-card"
+    :href="data.link"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <SZBlockContainer class="full">
+      <h3>{{ data.name }}</h3>
+    </SZBlockContainer>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+import { SZBlockContainer } from '@shelter-zone/shelter-ui'
+
+interface ResourceCardProps {
+  data: {
+    name: string
+    description: string
+    link: string
+  }
+}
+withDefaults(defineProps<ResourceCardProps>(), {
+  data: () => ({
+    name: '',
+    description: '',
+    link: '',
+  }),
+})
+</script>
+
+<style scoped lang="postcss">
+.resource-card {
+  @apply max-w-[400px] w-full;
+}
+</style>
