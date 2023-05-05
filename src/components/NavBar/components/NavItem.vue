14270a50af51eb52dba5a174b6efa606715bd960
Proladon <Proladon@gmail.com>
2023-05-05 10:12:57 +0800

    navitem button

--- src/components/NavBar/components/NavItem.vue
+++ src/components/NavBar/components/NavItem.vue
@@ -1,16 +1,23 @@
 <template>
-  <RouterLink :to="{ name: route.name }">
-    {{ get(route, 'meta.title') }}
+  <RouterLink class="nav-item" :to="{ name: route.name }">
+    <n-button quaternary type="primary">
+      {{ get(route, 'meta.title') }}
+    </n-button>
   </RouterLink>
 </template>
 
 <script setup lang="ts">
 import type { RouteRecordRaw } from 'vue-router'
 import { RouterLink } from 'vue-router'
+import { NButton } from 'naive-ui'
 import { get } from 'lodash-es'
 defineProps<{
   route: RouteRecordRaw
 }>()
 </script>
 
-<style scoped lang="postcss"></style>
+<style scoped lang="postcss">
+.nav-item {
+  @apply text-shadow-xl;
+}
+</style>
--- src/use/useRoutes.ts
+++ src/use/useRoutes.ts
@@ -1,11 +1,9 @@
-import { useOauthStore } from '@/stores/oauth'
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { get } from 'lodash-es'
 
 export const useRoutes = () => {
   const router = useRouter()
-  const oauthStore = useOauthStore()
   const routes = computed(() => router.options.routes)
   const displayRoutes = computed(() => {
     return router.options.routes.filter((route) => !get(route, 'meta.hidden'))
