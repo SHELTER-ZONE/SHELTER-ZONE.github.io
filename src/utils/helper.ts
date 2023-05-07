43a8937ffe3de946cd4e16eefb230ece640b890a
Proladon <Proladon@gmail.com>
2023-05-07 22:58:54 +0800

    [wip] userprofile

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,15 @@
 import { get } from 'lodash-es'
 import { useAppStore } from '@/stores/app'
+import dayjs from 'dayjs'
 
 export const getHost = (hostName: string) => {
   const appStore = useAppStore()
   return get(appStore.apiEndPoints, hostName)
+}
+
+export const dateFormat = (
+  dateTime: string,
+  dateFormat: string = 'YYYY/MM/DD HH:mm',
+): string => {
+  return dayjs(dateTime).format(dateFormat)
 }
