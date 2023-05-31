715d9840d244b3aa4ec3e6523b4e12bc1027269e
Proladon <Proladon@gmail.com>
2023-05-31 16:06:42 +0800

    [cmp] SZTeam - TeamBlock

---
+++ src/configs/team.ts
@@ -0,0 +1,13 @@
+import { Chemistry, FlashFilled, Bot } from '@vicons/carbon'
+
+export const teamTypeIconConfig = {
+  backend: Chemistry,
+  frontend: FlashFilled,
+  bot: Bot,
+}
+
+export const teamTypeColorConfig = {
+  backend: '#63E2B7',
+  frontend: '#F2E355',
+  bot: '#55B0F2',
+}
--- src/views/SZTeam/SZTeam.vue
+++ src/views/SZTeam/SZTeam.vue
@@ -1,38 +1,90 @@
 <template>
   <main class="sz-team">
-    <section>
-      <h1>Service Development Team</h1>
-      <SZBlockContainer class="w-fit">
-        <p class="text-center">Tech Stack</p>
-        <div class="flex flex-col gap-[20px] items-center">
-          <img
-            class="m-auto"
-            src="https://skillicons.dev/icons?i=vscode,docker,figma,vue"
-            alt=""
-          />
-          <img
-            src="https://skillicons.dev/icons?i=vite,express,nodejs,js"
-            alt=""
-          />
-          <img
-            src="https://skillicons.dev/icons?i=ts,html,css,windicss"
-            alt=""
-          />
-        </div>
-      </SZBlockContainer>
+    <PageTitle />
+
+    <section class="sz-team-intro">
+      <p>SZ 開發團隊屬於貢獻性質，開發 SZ 相關服務、網站、Discord Bot</p>
+      <p>casc</p>
     </section>
-    <section>
-      <h1>Bot Development Team</h1>
-    </section>
+
+    <div class="teams-list">
+      <TeamBlock
+        v-for="team in teams"
+        :key="team.name"
+        :team="team.name"
+        :techs="team.techs"
+        :type="team.type"
+      />
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { SZBlockContainer } from '@shelter-zone/shelter-ui'
+import TeamBlock from './components/TeamBlock.vue'
+import PageTitle from '@/components/Title/PageTitle.vue'
+
+const teams = [
+  {
+    name: '後端開發團隊',
+    type: 'backend',
+    techs: [
+      'vscode',
+      'docker',
+      'github',
+      'vite',
+      'express',
+      'nodejs',
+      'ts',
+      'mongodb',
+      'postman',
+    ],
+  },
+  {
+    name: '前端開發團隊',
+    type: 'frontend',
+    techs: [
+      'vscode',
+      'figma',
+      'github',
+      'vue',
+      'vite',
+      'js',
+      'ts',
+      'html',
+      'css',
+      'windicss',
+      'postman',
+    ],
+  },
+  {
+    name: 'Discord Bot 開發團隊',
+    type: 'bot',
+    techs: [
+      'discord',
+      'vscode',
+      'docker',
+      'github',
+      'vite',
+      'nodejs',
+      'ts',
+      'postman',
+    ],
+  },
+]
 </script>
 
 <style scoped lang="postcss">
 .sz-team {
   @apply viewPx pt-[150px];
+  @apply flex flex-col items-center;
+}
+
+.sz-team-intro {
+  @apply text-md mb-[40px];
+}
+
+.teams-list {
+  @apply flex flex-col items-center gap-[20px];
+  @apply tablet:(flex-wrap flex-row justify-center items-stretch);
 }
 </style>
